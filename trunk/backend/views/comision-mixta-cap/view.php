cd99a9ba4bb8c1a4120c12befea9f7007cfbee8a
rafanataren <rafanataren@bd112698-f8c9-4ad5-a464-942d7b2e0650>
2015-08-01 05:16:49 +0000

    cambio

--- trunk/backend/views/comision-mixta-cap/view.php
+++ trunk/backend/views/comision-mixta-cap/view.php
@@ -34,6 +34,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'NUMERO_INTEGRANTES',
             'FECHA_CONSTITUCION',
             'FECHA_ELABORACION',
+			'FECHA_AGREGO',
             'ACTIVO',
         ],
     ]) ?>
