ad41a09c19f9ea8947f225909bf78b6ca414ae46
jjnataren <jjnataren@bd112698-f8c9-4ad5-a464-942d7b2e0650>
2015-11-04 06:02:45 +0000

    Se agrega el nuevo rol para usuarios de tipo instructor

--- trunk/common/models/User.php
+++ trunk/common/models/User.php
@@ -22,29 +22,26 @@ use yii\web\IdentityInterface;
  * @property integer $created_at
  * @property integer $updated_at
  * @property string $password write-only password
- *
- * @property Article[] $articles
- * @property EmpresaUsuario[] $empresaUsuarios
- * @property Empresa[] $iDEMPRESAs
- * @property UserProfile $userProfile
  */
 class User extends ActiveRecord implements IdentityInterface
 {
-	const STATUS_DELETED = 0;
-	const STATUS_ACTIVE = 1;
-	
-	const ROLE_USER = 1;
-	const ROLE_MANAGER = 5;
-	const ROLE_ADMINISTRATOR = 10;
-	
-	const EVENT_AFTER_SIGNUP = 'afterSignup';
-	const EVENT_AFTER_LOGIN = 'afterLogin';	
+    const STATUS_DELETED = 0;
+    const STATUS_ACTIVE = 1;
+
+    const ROLE_USER = 1;
+    const ROLE_MANAGER = 5;
+    const ROLE_ADMINISTRATOR = 10;
+    const ROLE_INSTRUCTOR = 7;
+
+    const EVENT_AFTER_SIGNUP = 'afterSignup';
+    const EVENT_AFTER_LOGIN = 'afterLogin';
+
     /**
      * @inheritdoc
      */
     public static function tableName()
     {
-        return 'tbl_user';
+        return '{{%user}}';
     }
 
     /**
@@ -52,25 +49,26 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public function behaviors()
     {
-    	return [
-    			TimestampBehavior::className(),
-    	];
+        return [
+            TimestampBehavior::className(),
+        ];
     }
-    
+
     /**
      * @return array
      */
     public function scenarios(){
-    	return ArrayHelper::merge(
-    			parent::scenarios(),
-    			[
-    					'oauth_create'=>[
-    							'oauth_client', 'oauth_client_user_id', 'email', 'username', '!status', '!role'
-    					]
-    			]
-    	);
+        return ArrayHelper::merge(
+            parent::scenarios(),
+            [
+                'oauth_create'=>[
+                    'oauth_client', 'oauth_client_user_id', 'email', 'username', '!status', '!role'
+                ]
+            ]
+        );
     }
-    
+
+
     /**
       * @inheritdoc
       */
@@ -82,7 +80,7 @@ class User extends ActiveRecord implements IdentityInterface
              ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
 
              ['role', 'default', 'value' => self::ROLE_USER],
-             ['role', 'in', 'range' => [self::ROLE_USER, self::ROLE_MANAGER, self::ROLE_ADMINISTRATOR]],
+             ['role', 'in', 'range' => [self::ROLE_USER, self::ROLE_MANAGER, self::ROLE_ADMINISTRATOR,  self::ROLE_INSTRUCTOR]],
          ];
      }
 
@@ -98,25 +96,25 @@ class User extends ActiveRecord implements IdentityInterface
     }
 
     public function getProfile(){
-    	return $this->hasOne(UserProfile::className(), ['user_id'=>'id']);
+        return $this->hasOne(UserProfile::className(), ['user_id'=>'id']);
     }
-    
+
     /**
      * @inheritdoc
      */
     public static function findIdentity($id)
     {
-    	return static::findOne($id);
+        return static::findOne($id);
     }
-    
+
     /**
      * @inheritdoc
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-    	throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
     }
-    
+
     /**
      * Finds user by username
      *
@@ -125,9 +123,9 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findByUsername($username)
     {
-    	return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
     }
-    
+
     /**
      * Finds user by password reset token
      *
@@ -136,44 +134,44 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findByPasswordResetToken($token)
     {
-    	$expire = 60 * 60 * 24; // 1 day
-    	$parts = explode('_', $token);
-    	$timestamp = (int) end($parts);
-    	if ($timestamp + $expire < time()) {
-    		// token expired
-    		return null;
-    	}
-    
-    	return static::findOne([
-    			'password_reset_token' => $token,
-    			'status' => self::STATUS_ACTIVE,
-    	]);
+        $expire = 60 * 60 * 24; // 1 day
+        $parts = explode('_', $token);
+        $timestamp = (int) end($parts);
+        if ($timestamp + $expire < time()) {
+            // token expired
+            return null;
+        }
+
+        return static::findOne([
+            'password_reset_token' => $token,
+            'status' => self::STATUS_ACTIVE,
+        ]);
     }
-    
+
     /**
      * @inheritdoc
      */
     public function getId()
     {
-    	return $this->getPrimaryKey();
+        return $this->getPrimaryKey();
     }
-    
+
     /**
      * @inheritdoc
      */
     public function getAuthKey()
     {
-    	return $this->auth_key;
+        return $this->auth_key;
     }
-    
+
     /**
      * @inheritdoc
      */
     public function validateAuthKey($authKey)
     {
-    	return $this->getAuthKey() === $authKey;
+        return $this->getAuthKey() === $authKey;
     }
-    
+
     /**
      * Validates password
      *
@@ -182,9 +180,9 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public function validatePassword($password)
     {
-    	return Yii::$app->getSecurity()->validatePassword($password, $this->password_hash);
+        return Yii::$app->getSecurity()->validatePassword($password, $this->password_hash);
     }
-    
+
     /**
      * Generates password hash from password and sets it to the model
      *
@@ -192,115 +190,84 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public function setPassword($password)
     {
-    	$this->password_hash = Yii::$app->getSecurity()->generatePasswordHash($password);
+        $this->password_hash = Yii::$app->getSecurity()->generatePasswordHash($password);
     }
-    
+
     /**
      * Generates "remember me" authentication key
      */
     public function generateAuthKey()
     {
-    	$this->auth_key = Yii::$app->getSecurity()->generateRandomString();
+        $this->auth_key = Yii::$app->getSecurity()->generateRandomString();
     }
-    
+
     /**
      * Generates new password reset token
      */
     public function generatePasswordResetToken()
     {
-    	$this->password_reset_token = Yii::$app->getSecurity()->generateRandomString() . '_' . time();
+        $this->password_reset_token = Yii::$app->getSecurity()->generateRandomString() . '_' . time();
     }
-    
+
     /**
      * Removes password reset token
      */
     public function removePasswordResetToken()
     {
-    	$this->password_reset_token = null;
+        $this->password_reset_token = null;
     }
-    
+
     /**
      * Returns user roles list
      * @param bool $role
      * @return array|mixed
      */
     public static function getRoles($role = false){
-    	$roles = [
-    			self::ROLE_USER=>Yii::t('common', 'User'),
-    			self::ROLE_MANAGER=>Yii::t('common', 'Manager'),
-    			self::ROLE_ADMINISTRATOR=>Yii::t('common', 'Administrator'),
-    	];
-    	return $role ? ArrayHelper::getValue($roles, $role) : $roles;
+        $roles = [
+            self::ROLE_USER=>Yii::t('common', 'User'),
+            self::ROLE_MANAGER=>Yii::t('common', 'Manager'),
+            self::ROLE_ADMINISTRATOR=>Yii::t('common', 'Administrator'),
+        	self::ROLE_INSTRUCTOR=>Yii::t('common', 'Instructor'),
+        ];
+        return $role ? ArrayHelper::getValue($roles, $role) : $roles;
     }
-    
+
     /**
      * Returns user statuses list
      * @param bool $status
      * @return array|mixed
      */
     public static function getStatuses($status = false){
-    	$statuses = [
-    			self::STATUS_ACTIVE=>Yii::t('common', 'Active'),
-    			self::STATUS_DELETED=>Yii::t('common', 'Deleted')
-    	];
-    	return $status ? ArrayHelper::getValue($statuses, $status) : $statuses;
+        $statuses = [
+            self::STATUS_ACTIVE=>Yii::t('common', 'Active'),
+            self::STATUS_DELETED=>Yii::t('common', 'Deleted')
+        ];
+        return $status ? ArrayHelper::getValue($statuses, $status) : $statuses;
     }
-    
+
     /**
      * Creates user profile and application event
      */
     public function afterSignup(){
-    	SystemEvent::log('user', self::EVENT_AFTER_SIGNUP, [
-    			'username'=>$this->username,
-    			'email'=>$this->email,
-    			'created_at'=>$this->created_at
-    	]);
-    	$profile = new UserProfile();
-    	$profile->locale = Yii::$app->language;
-    	$this->link('profile', $profile);
-    	$this->trigger(self::EVENT_AFTER_SIGNUP);
+        SystemEvent::log('user', self::EVENT_AFTER_SIGNUP, [
+            'username'=>$this->username,
+            'email'=>$this->email,
+            'created_at'=>$this->created_at
+        ]);
+        $profile = new UserProfile();
+        $profile->locale = Yii::$app->language;
+        $this->link('profile', $profile);
+        $this->trigger(self::EVENT_AFTER_SIGNUP);
     }
-    
+
     public function getPublicIdentity()
     {
-    	if($this->profile && $this->profile->getFullname()){
-    		return $this->profile->getFullname();
-    	}
-    	if($this->username){
-    		return $this->username;
-    	}
-    	return $this->email;
-    }
-    
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getArticles()
-    {
-        return $this->hasMany(Article::className(), ['updater_id' => 'id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getEmpresaUsuarios()
-    {
-        return $this->hasMany(EmpresaUsuario::className(), ['ID_USUARIO' => 'id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getIDEMPRESAs()
-    {
-        return $this->hasMany(Empresa::className(), ['ID_EMPRESA' => 'ID_EMPRESA'])->viaTable('tbl_empresa_usuario', ['ID_USUARIO' => 'id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getUserProfile()
-    {
-        return $this->hasOne(UserProfile::className(), ['user_id' => 'id']);
+        if($this->profile && $this->profile->getFullname()){
+            return $this->profile->getFullname();
+        }
+        if($this->username){
+            return $this->username;
+        }
+        return $this->email;
     }
 }
