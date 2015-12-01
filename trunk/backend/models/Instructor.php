7fb5e7121fd5aa0829dc83e857db08451bc30128
jjnataren <jjnataren@bd112698-f8c9-4ad5-a464-942d7b2e0650>
2015-12-01 04:11:58 +0000

    se agrea campo para almacenar el documento probatorio

--- trunk/backend/models/Instructor.php
+++ trunk/backend/models/Instructor.php
@@ -35,6 +35,25 @@ use Yii;
  */
 class Instructor extends \yii\db\ActiveRecord
 {
+	/**
+	 * Tipos de instructores
+	 * @var unknown
+	 */
+	const TIPO_AGENTE_CAP_INTERNO = 1;
+	const TIPO_AGENTE_EXT_IND = 2;
+	const TIPO_AGENTE_ACREDITACION = 3;
+	const TIPO_AGENTE_PROVEDOR = 4;
+	
+	
+	public static function getTypeInstructor(){
+			
+		return [Instructor::TIPO_AGENTE_CAP_INTERNO=>'Agente capacitador interno',
+				self::TIPO_AGENTE_EXT_IND=>'Agente capacitador externo con numero de acreditación, independiente',
+				self::TIPO_AGENTE_ACREDITACION=>'Agente capacitador externo con numero de acreditación, perteneciente a una empresa ',
+				self::TIPO_AGENTE_PROVEDOR=>'Agente empresa provedor externo'];
+	}
+	
+    
     /**
      * @inheritdoc
      */
@@ -43,7 +62,7 @@ class Instructor extends \yii\db\ActiveRecord
         return 'tbl_instructor';
     }
 
-    /**
+  /**
      * @inheritdoc
      */
     public function rules()
@@ -55,39 +74,35 @@ class Instructor extends \yii\db\ActiveRecord
             [['DOMICILIO', 'CORREO_ELECTRONICO'], 'string', 'max' => 300],
             [['COMENTARIOS'], 'string', 'max' => 200],
             [['RFC'], 'string', 'max' => 13],
-            [['SIGN_PIC', 'SIGN_PASSWD'], 'string', 'max' => 2048],
-            [['DOCUMENTO_PROBATORIO'], 'string', 'max' => 1048]
+            [['SIGN_PIC', 'SIGN_PASSWD'], 'string', 'max' => 2048]
         ];
     }
 
-    /**
+   /**
      * @inheritdoc
      */
     public function attributeLabels()
     {
-        return [
-            'ID_INSTRUCTOR' => 'Id  Instructor',
-            'ID_EMPRESA' => 'Id  Empresa',
-            'NOMBRE_AGENTE_EXTERNO' => 'Nombre  Agente  Externo',
-            'NOMBRE' => 'Nombre',
-            'APP' => 'App',
-            'APM' => 'Apm',
-            'DOMICILIO' => 'Domicilio',
-            'TELEFONO' => 'Telefono',
-            'CORREO_ELECTRONICO' => 'Correo  Electronico',
-            'LOGOTIPO' => 'Logotipo',
-            'NUM_REGISTRO_AGENTE_EXTERNO' => 'Num  Registro  Agente  Externo',
-            'TIPO_INSTRUCTOR' => 'Tipo  Instructor',
-            'COMENTARIOS' => 'Comentarios',
-            'ACTIVO' => 'Activo',
-            'ID_USUARIO' => 'Id  Usuario',
-            'RFC' => 'Rfc',
-            'SIGN_PIC' => 'Sign  Pic',
-            'SIGN_PASSWD' => 'Sign  Passwd',
-            'SIGN_PIC_EXTENSION' => 'Sign  Pic  Extension',
-            'SIGN_CREATED_AT' => 'Sign  Created  At',
-            'DOCUMENTO_PROBATORIO' => 'Documento  Probatorio',
-        ];
+    	return [
+    			'ID_INSTRUCTOR' => 'Id instructor',
+    			'ID_EMPRESA' => 'Id  Empresa',
+    			'NOMBRE_AGENTE_EXTERNO' => 'Nombre del agente  externo',
+    			'NOMBRE' => 'Nombre',
+    			'APP' => 'Apellido paterno',
+    			'APM' => 'Apellido materno',
+    			'DOMICILIO' => 'Domicilio',
+    			'TELEFONO' => 'Teléfono',
+    			'CORREO_ELECTRONICO' => 'Correo  electrónico',
+    			'LOGOTIPO' => 'Logotipo',
+    			'NUM_REGISTRO_AGENTE_EXTERNO' => 'Número de registro del agente  externo',
+    			'TIPO_INSTRUCTOR' => 'Tipo de instructor',
+    			'COMENTARIOS' => 'Comentarios',
+    			'ACTIVO' => 'Activo',
+    			'RFC'=> 'RFC',
+    			'SIGN_PICTURE' => 'Imagen firma',
+    			'SIGN_PASSWD' => 'Constraseña encriptación',
+    			'SIGN_KEY' => 'Sign  Key',
+    	];
     }
 
     /**
@@ -113,4 +128,56 @@ class Instructor extends \yii\db\ActiveRecord
     {
         return $this->hasOne(User::className(), ['id' => 'ID_USUARIO']);
     }
+    
+    
+    /**
+     * Get instance of Instructor
+     * @throws NotFoundHttpException
+     */
+    public static function getOwnData() {
+    
+    	$empresaModel = EmpresaUsuario::getMyCompany();
+    
+    	$model = Instructor::findOne(['ID_USUARIO'=>Yii::$app->user->id, 'ID_EMPRESA'=>$empresaModel->ID_EMPRESA]);
+    
+    	if($model === null) throw new NotFoundHttpException('The requested page does not exist.');
+    
+    	return $model;
+    }
+    /**
+     * Gets  singning image binary base64
+     */
+    public function getSigningBinary(){
+    
+    	/*
+    	 * into a reproducable iv/key pair
+    	 */
+    
+    	$image64Data = null;
+    
+    
+    	if($this->SIGN_PIC !== null){
+    
+    		 
+    		$passphrase  =  $this->SIGN_PASSWD;
+    		 
+    		$iv = substr(md5('iv'.$passphrase, true), 0, 8);
+    		$key = substr(md5('pass1'.$passphrase, true) .
+    				md5('pass2'.$passphrase, true), 0, 24);
+    		$opts = array('iv'=>$iv, 'key'=>$key);
+    		 
+    		$fp = fopen($this->SIGN_PIC, 'r');
+    		stream_filter_append($fp, 'mdecrypt.tripledes', STREAM_FILTER_READ, $opts);
+    		$data = rtrim(stream_get_contents($fp));
+    		fclose($fp);
+    		 
+    		$image64Data =  $data;
+    		 
+    	}
+    
+    	return base64_encode($image64Data);
+    
+    }
+    }
+    
 }
